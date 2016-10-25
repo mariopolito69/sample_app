b108a33d55eb947a8d19189f5bdda87949ba2fad
Mario Polito <mariopolito69@gmail.com>
2016-10-25 23:22:21 +0200

    Aggiunto il test e le pagine un po dinamiche

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,12 @@
 module StaticPagesHelper
+
+  # Returns the full title on a per-page basis.
+  def fullTitle(page_title = '')
+    base_title = "tette"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
