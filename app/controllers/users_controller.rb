8b7744129ac7650124272620f835f603f05ca7b4
Mario Polito <mariopolito69@gmail.com>
2016-11-22 10:40:43 +0000

    signup funzionante

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    user_params = params.require(:user).permit(:name,:email,:password, :password_confirmation)
+    @user = User.new(user_params)
     if @user.save
-      
+      render 'show'
     else
       render 'new'
     end
   end
+  
+  def user_params
+
+  end
+  
   
 end
